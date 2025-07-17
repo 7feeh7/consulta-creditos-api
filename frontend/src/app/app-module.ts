3bd10d7b34f3b78dba384b752cfa3efafb5ab81f
Felipe Pires Soares <felipe.pires.soaresti@gmail.com>
2025-07-17 19:45:50 -0300

    feat: setup inicial do projeto com bootstrap

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ConsultaCreditosComponent } from './components/consulta-creditos/consulta-creditos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    App
+    App,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConsultaCreditosComponent
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
